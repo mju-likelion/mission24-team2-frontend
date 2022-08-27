60338ab427d76b8bd4c35b23f7e28fe6b1c33524
Bhanjo <hanjo1515@naver.com>
2022-08-27 23:53:45 +0900

    feat: 홈 컴포넌트 제작

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,36 @@
+import styled from '@emotion/styled';
+import Buttons from '../../components/Button';
 import PageContainer from '../../components/PageContainer';
+import Logo from '../../img/svg/Logo';
 
 const Home = () => {
-  return <PageContainer>홈입니다</PageContainer>;
+  return (
+    <PageContainer>
+      <Content>
+        <ContentBox>
+          <Logo />
+          <Buttons width='464px' height='117px' fontSize='50px'>
+            시작하기
+          </Buttons>
+        </ContentBox>
+      </Content>
+    </PageContainer>
+  );
 };
+
+const Content = styled.div`
+  width: 100%;
+  height: inherit;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ContentBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 75px;
+`;
 
 export default Home;
